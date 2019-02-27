ce85b528e2143ae513ea5683af1a7bcc932e0a0c
wangjue1 <wangjue1@ifeng.com>
2019-02-27 14:35:08 +0800

    添加_id获取方式示例

--- GO/Example/mongo.go
+++ GO/Example/mongo.go
@@ -2,12 +2,15 @@ package main
 
 // MongoDB连接并获取数据
 import (
+	//"encoding/hex"
 	"fmt"
 	"labix.org/v2/mgo"
 	"labix.org/v2/mgo/bson"
 )
 
 type Person struct {
+	//ID     []byte        `bson:"_id"` //也可以用这种方式获取,但是要用"encoding/hex" 解析
+	IDNew  bson.ObjectId `bson:"_id"` //推荐的_id字段获取方式
 	GUID   int
 	ENERGY int `bson:"energy_score"` //表示mongodb数据库中对应的字段名称
 }
@@ -42,6 +45,11 @@ func main() {
 
 	for iter.Next(&result) {
 		fmt.Printf("guid: %v, energy:%v\n", result.GUID, result.ENERGY)
+		// id := hex.EncodeToString(result.ID)
+		// fmt.Printf("_id: %v\n", id)
+		fmt.Printf("_id new: %v\n", result.IDNew)        //ObjectIdHex("5ae442265142a3df768b4569") 类型的值
+		hexid := fmt.Sprintf("%x", string(result.IDNew)) //ObjectIdHex 转string
+		fmt.Println(hexid)
 	}
 	fmt.Println("========end task=========")
 }
